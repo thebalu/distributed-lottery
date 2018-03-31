96a62e9d28cf425263548eba9a2635e87d608de5
Balazs Varga <thebalu1@gmail.com>
2018-03-31 14:53:13 +0200

    trying some tests

--- test/LotteryTest.js
+++ test/LotteryTest.js
@@ -3,11 +3,22 @@ var Lottery = artifacts.require("Lottery");
 contract ('Lottery', (accounts) => {
 
   var creatorAddress = accounts[0];
+  var contractAddress;
 
-  Lottery.deployed().then( instance => {
-    contractAddress = instance.address;
+  it('should set the owner correctly', () => {
+    //var contractInstance = await Lottery.deployed();
+    var contractInstance;
+    return Lottery.deployed().then( instance => {
+      contractInstance = instance;
+    }).then( result => {
+      assert.equal(contractInstance.address, "0123", "should fail");
+    });
   });
 
+  it('do the same with await', async () => {
+    var contractInstance = await Lottery.deployed();
+    assert.equal(contractInstance.address, "0x0", "this should also fail");
 
+  });
 
-} );
+});
