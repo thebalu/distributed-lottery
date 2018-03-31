7992caf35bad3bb073b4244e5e4fc906fe2dacae
Balazs Varga <thebalu1@gmail.com>
2018-03-31 14:57:30 +0200

    add an actual test for owner

--- test/LotteryTest.js
+++ test/LotteryTest.js
@@ -5,20 +5,28 @@ contract ('Lottery', (accounts) => {
   var creatorAddress = accounts[0];
   var contractAddress;
 
-  it('should set the owner correctly', () => {
-    //var contractInstance = await Lottery.deployed();
-    var contractInstance;
-    return Lottery.deployed().then( instance => {
-      contractInstance = instance;
-    }).then( result => {
-      assert.equal(contractInstance.address, "0123", "should fail");
-    });
+  it('should set the owner correctly', async () => {
+    contractInstance = await Lottery.deployed();
+    assert.equal(contractInstance.owner, accounts[0]);
   });
 
-  it('do the same with await', async () => {
-    var contractInstance = await Lottery.deployed();
-    assert.equal(contractInstance.address, "0x0", "this should also fail");
-
-  });
 
 });
+
+
+// For reference:
+// it('should set the owner correctly', () => {
+//   //var contractInstance = await Lottery.deployed();
+//   var contractInstance;
+//   return Lottery.deployed().then( instance => {
+//     contractInstance = instance;
+//   }).then( result => {
+//     assert.equal(contractInstance.address, "0123", "should fail");
+//   });
+// });
+//
+// it('do the same with await', async () => {
+//   var contractInstance = await Lottery.deployed();
+//   assert.equal(contractInstance.address, "0x0", "this should also fail");
+//
+// });
